cd5ddcfac67adf504766abf231600e8a765a5639
Chapman-John <52362698+Chapman-John@users.noreply.github.com>
2024-04-23 16:41:05 -0700

    js to tsx conversion

--- full-stack-serverless/src/App.tsx
+++ full-stack-serverless/src/App.tsx
@@ -1,7 +1,7 @@
-import './App.css';
+import React from 'react';
+import './app.css';
 
-function App()
-{
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
